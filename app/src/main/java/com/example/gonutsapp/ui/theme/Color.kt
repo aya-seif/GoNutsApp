60716e3c8b88cba2007f0236280fd5680d29426e
AyaAbdelaiz <96724374+AyaAbdelaiz@users.noreply.github.com>
2023-07-15 15:28:34 +0300

    Initial commit

---
+++ app/src/main/java/com/example/gonutsapp/ui/theme/Color.kt
@@ -0,0 +1,18 @@
+package com.example.gonutsapp.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
+
+val Primary = Color(0xFFFF7074)
+val Secondary = Color(0xFFFED8DF)
+val Tertiary = Color(0xFFFF9494)
+val White = Color(0xFFFFFFFF)
+val Black = Color(0xFF000000)
+val Grey = Color(0xFF585858)
---
+++ app/src/main/java/com/example/gonutsapp/ui/theme/Styke.kt
@@ -0,0 +1,86 @@
+package com.example.gonutsapp.ui.theme
+
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.unit.sp
+
+val HeadlineLargeSemiBold = TextStyle(
+    fontSize = 54.sp,
+    fontFamily = Inter,
+    fontWeight = FontWeight.SemiBold,
+    color = Primary,
+)
+
+val TitleMedium = TextStyle(
+    fontSize = 18.sp,
+    fontFamily = Inter,
+    fontWeight = FontWeight.Medium,
+    color = Grey,
+)
+
+val TitleLargeSemiBold = TextStyle(
+    fontSize = 20.sp,
+    fontFamily = Inter,
+    fontWeight = FontWeight.SemiBold,
+    color = Black,
+)
+
+val HeadlineSmallSemiBold = TextStyle(
+    fontSize = 30.sp,
+    fontFamily = Inter,
+    fontWeight = FontWeight.SemiBold,
+    color = Primary,
+)
+
+val BodyMedium = TextStyle(
+    fontSize = 14.sp,
+    fontFamily = Inter,
+    fontWeight = FontWeight.Medium,
+    color = Grey,
+)
+
+val TitleSmallMedium = TextStyle(
+    fontSize = 16.sp,
+    fontFamily = Inter,
+    fontWeight = FontWeight.Medium,
+    color = Black,
+)
+
+val BodySmallNormal =  TextStyle(
+    fontSize = 12.sp,
+    fontFamily = Inter,
+    fontWeight = FontWeight.Normal,
+    color = Grey,
+)
+
+val TitleXLargeSemiBold = TextStyle(
+    fontSize = 22.sp,
+    fontFamily = Inter,
+    fontWeight = FontWeight.SemiBold,
+    color = Black,
+)
+
+val BodyMediumSemiBold = TextStyle(
+    fontSize = 14.sp,
+    fontFamily = Inter,
+    fontWeight = FontWeight.SemiBold,
+    color = Grey,
+    textDecoration = TextDecoration.LineThrough,
+)
+
+val BodyMediumNormal = TextStyle(
+    fontSize = 14.sp,
+    fontFamily = Inter,
+    fontWeight = FontWeight.Normal,
+    color = Black,
+    letterSpacing = 0.7.sp,
+)
+
+val TitleXLarge = TextStyle(
+    fontSize = 22.sp,
+    fontFamily = Inter,
+    fontWeight = FontWeight.Medium,
+    color = Black,
+)
+
